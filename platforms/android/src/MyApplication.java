f9597d73751374f395e75264fbc3ca6d88c1934e
Kunio SAWATANI <devel@fathens.org>
2014-08-21 15:57:16 +0900

    ACRA-4.5.0 を付加

--- platforms/android/src/MyApplication.java
+++ platforms/android/src/MyApplication.java
@@ -1,6 +1,16 @@
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
 import android.app.Application;
 
-
+@ReportsCrashes(formKey = "")
 public class MyApplication extends Application {
 
+    @Override
+    public void onCreate() {
+	super.onCreate();
+
+	// The following line triggers the initialization of ACRA
+	ACRA.init(this);
+    }
 }
