4e53dc96a4a28afe37c4b17114b5d9c271568211
Kunio Sawatani <devel@fathens.org>
2015-02-21 14:46:24 +0900

    固定値は annotation に移動

--- platforms/android/src/org/fathens/cordova/acra/AcraApplication.java
+++ platforms/android/src/org/fathens/cordova/acra/AcraApplication.java
@@ -5,12 +5,15 @@ import org.acra.ACRAConfiguration;
 import org.acra.ACRAConfigurationException;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
-import org.acra.sender.EmailIntentSender;
 
 import android.app.Application;
 import android.util.Log;
 
-@ReportsCrashes(formKey = "")
+@ReportsCrashes(
+        mode = ReportingInteractionMode.TOAST,
+        httpMethod = org.acra.sender.HttpSender.Method.PUT,
+        reportType = org.acra.sender.HttpSender.Type.JSON,
+        formKey = "")
 public class AcraApplication extends Application {
     static final String TAG = "ACRA_Application";
 
@@ -27,25 +30,17 @@ public class AcraApplication extends Application {
         super.onCreate();
 
         // Initialization of ACRA
-        final ReportingInteractionMode mode = ReportingInteractionMode.TOAST;
         final int toastText = getId("toast_text");
         final String url = getText("url");
         final String username = getText("username");
         final String password = getText("password");
-        Log.d(TAG, String.format("Configuration Setup: MODE=%s, TOAST_TEXT=%x, PUT_URI='%s'", mode, toastText, url));
-        try {
-            final ACRAConfiguration config = ACRA.getNewDefaultConfig(this);
-            config.setMode(mode);
-            config.setResToastText(toastText);
-            config.setHttpMethod(org.acra.sender.HttpSender.Method.PUT);
-            config.setReportType(org.acra.sender.HttpSender.Type.JSON);
-            config.setFormUri(url);
-            config.setFormUriBasicAuthLogin(username);
-            config.setFormUriBasicAuthPassword(password);
-            ACRA.setConfig(config);
-            ACRA.init(this);
-        } catch (ACRAConfigurationException ex) {
-            throw new RuntimeException(ex);
-        }
+        Log.d(TAG, String.format("Configuration Setup: PUT_URI='%s'", url));
+        final ACRAConfiguration config = ACRA.getNewDefaultConfig(this);
+        config.setResToastText(toastText);
+        config.setFormUri(url);
+        config.setFormUriBasicAuthLogin(username);
+        config.setFormUriBasicAuthPassword(password);
+        ACRA.setConfig(config);
+        ACRA.init(this);
     }
 }
